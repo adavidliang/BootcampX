88556e9eaf05fc6f663b4ec501947e227144e3f6
adavidliang <adavidliang@gmail.com>
2023-06-28 23:31:00 -0400

    avoid injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,16 +12,19 @@ const client = new Client({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+const quertString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+const teacher = process.argv[2]
+const values = [`${teacher}`]
+pool.query(quertString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}:${user.teacher}`);
